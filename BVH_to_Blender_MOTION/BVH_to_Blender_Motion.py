a1ce43998022a80c44913051a39f27847dd9e2c0
KWARK MINCHANG <kwarkmc99@gmail.com>
2023-08-25 11:32:23 +0900

    SQL을 통한 bvh 파일 다운로드를 위한 Blender 파이프라인 수정

--- BVH_to_Blender_MOTION/BVH_to_Blender_Motion.py
+++ BVH_to_Blender_MOTION/BVH_to_Blender_Motion.py
@@ -4,6 +4,23 @@ import bpy, time, os, shutil
 bvh_folder_path = "C:/Users/054/Desktop/BVH_files"
 files = [f for f in os.listdir(bvh_folder_path) if os.path.isfile(os.path.join(bvh_folder_path, f))]
 
+# DB 설정
+import pyodbc
+import datetime
+
+#Azure SQL DB Initialization 부분
+server='tcp:sql-3dchatbot-server.database.windows.net,1433'
+database='3D-ChatbotDB'
+username='SKT1'
+password=''
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password+';Encrypt=yes;TrustServerCertificate=no;')
+
+cursor = conn.cursor()
+
+#DB 내의 가장 최근 파일을 불러오기 위해 2000년 1월 1일을 기준으로 설정
+last_timestamp = datetime.datetime(2000, 1, 1)
+
 # 메인 캐릭터, 시작 애니메이션 파일 위치는 BVH 파일 폴더와 다른 곳이어야 함
 adot = bpy.data.objects.get("HelloBlender") # 기본 캐릭터 
 adot_name = "HelloBlender"
@@ -60,6 +77,38 @@ animation_on = True
 def main_timer():
     global bvh_folder_path, files, animation_on, adot, anilist
     print(int((time.time()-startTime)*10)/10, end="\r") # 프로그램 켜놓은 시간 체크
+
+    insert_query = """
+    SELECT * FROM BVHdb WHERE Timestamp > ? ORDER BY Timestamp DESC
+    """
+
+    cursor.execute(insert_query, last_timestamp)
+    rows = cursor.fetchall()
+
+    data_id = rows[0]
+    timestamp = rows[1]
+    motion_content = rows[2]
+    label_name = rows[3]
+    file_name = rows[4]
+    file_data = rows[5]
+
+    last_timestamp = timestamp
+
+    bvh_file_path = f"C:/Users/054/Desktop/BVH_files/{file_name}.bvh"
+
+    with open(bvh_file_path, 'wb') as file:
+        file.write(file_data)
+
+    print(f"""
+            DATA DOWNLOADED!!
+            DataID:          {data_id}
+            Timestamp:       {timestamp}
+            MotionContent:   {motion_content}
+            LabelName:       {label_name}
+            FileName:        {file_name}
+            FileData (size): {len(file_data) if file_data else 0} bytes
+            """)
+
     files = [f for f in os.listdir(bvh_folder_path) if os.path.isfile(os.path.join(bvh_folder_path, f))]
     if len(files) > 1:
         files.sort(key=lambda f: os.path.getmtime(os.path.join(bvh_folder_path, f)))
